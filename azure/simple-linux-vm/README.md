82e184bb3e170fd4b24e45d6bc5a74af28376f4e
HashedDan <daniel.mangum@slalom.com>
2019-01-15 17:19:48 -0600

    azure linux vm

---
+++ azure/simple-linux-vm/README.md
@@ -0,0 +1,1 @@
+# Provision a Single Linux VM on Azure
---
+++ azure/simple-linux-vm/main.tf
@@ -0,0 +1,8 @@
+# Define provider as Azure
+provider "azurerm" {
+  # These can also be defined as environment variables
+  #   subscription_id = "yoursubscriptionid"
+  #   client_id       = "yourclientid"
+  #   client_secret   = "yourclientsecret"
+  #   tenant_id       = "yourtenantid"
+}
